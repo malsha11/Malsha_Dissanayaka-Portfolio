7141f72bbf4c46f7601212dcba7d87aef3d40b12
malsha11 <84215169+malsha11@users.noreply.github.com>
2022-02-27 17:57:32 +0000

    Deploying to gh-pages from  @ 37e505201518dd5c7f79870ba08e321233e516bb 🚀

---
+++ precache-manifest.26de203a85b8437c20141257fe537414.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c822d6cbf1873ca7939eb0bd0e422e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "885b5aa8937af7c99d9a",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "c94662e04e2fc10346da",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "28ce96f7dd8d933354cb",
+    "url": "/static/css/main.ea0c94ea.chunk.css"
+  },
+  {
+    "revision": "eca1e6854b9e04a14045",
+    "url": "/static/js/2.44800d34.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.44800d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885b5aa8937af7c99d9a",
+    "url": "/static/js/3.fb82432b.chunk.js"
+  },
+  {
+    "revision": "c94662e04e2fc10346da",
+    "url": "/static/js/4.011c95b8.chunk.js"
+  },
+  {
+    "revision": "28ce96f7dd8d933354cb",
+    "url": "/static/js/main.5d389939.chunk.js"
+  },
+  {
+    "revision": "9a1a67b2585d7dcd328c",
+    "url": "/static/js/runtime-main.58e0a14f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e97a97b22376ca3f0ac7ddc52aba271a",
+    "url": "/static/media/Cetificate02.e97a97b2.jpeg"
+  },
+  {
+    "revision": "9fa1890b81f8b260f1a3bc571e8050c3",
+    "url": "/static/media/Cetificate03.9fa1890b.jpg"
+  },
+  {
+    "revision": "fbc3da92afe3b3767e90326c14059d15",
+    "url": "/static/media/CiscoCetificate.fbc3da92.jpg"
+  },
+  {
+    "revision": "1707e1584b7bec5fd751a52f7a2ad60b",
+    "url": "/static/media/CoffeTableBook.1707e158.jpg"
+  },
+  {
+    "revision": "05cf5183c0fc6a45939a2c3edcf96e85",
+    "url": "/static/media/FOSS_Logo.05cf5183.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0e35f2a6ae3f942b374754333ba3e2d1",
+    "url": "/static/media/NewsApp.0e35f2a6.png"
+  },
+  {
+    "revision": "49e1799c565b3c05d14b59f9fe39f88b",
+    "url": "/static/media/NewsAppUi.49e1799c.png"
+  },
+  {
+    "revision": "165f2f30a739e467eed967caf1f4b668",
+    "url": "/static/media/PeoplesBankLogo.165f2f30.jpg"
+  },
+  {
+    "revision": "72e5dc55da26dba1b8c575482e6a08dc",
+    "url": "/static/media/Web.72e5dc55.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "890611175eddea172dba12702e618e00",
+    "url": "/static/media/usjp-logo.89061117.png"
+  }
+]);
